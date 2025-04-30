bfdd56cfc8a5e3b13459b64fcac5c3d5422c43be
sasaSilver <al.mikhailov@innopolis.university>
2025-04-30 04:27:01 +0300

    change client layout

--- src/client.py
+++ src/client.py
@@ -2,67 +2,68 @@ import streamlit as st
 import grpc
 from cryptography.hazmat.primitives.asymmetric import ed25519
 
+import proto.phonebook_pb2_grpc as phonebook_pb2_grpc
 from proto.phonebook_pb2 import (
     LookupRequest, LookupResponse,
     AddEntryRequest, AddEntryResponse
 )
-import proto.phonebook_pb2_grpc as phonebook_pb2_grpc
 
 SERVER_ADDRESS = "localhost:50051"
 PUBKEY = ed25519.Ed25519PublicKey.from_public_bytes(
-            bytes.fromhex(st.secrets["SERVER_PUBLIC_KEY"])
+    bytes.fromhex(st.secrets["SERVER_PUBLIC_KEY"])
 )
 
 def verify_signature(name: str, number: str, signature: bytes):
+    signed_data = f"{name}:{number}".encode()
     try:
-        signed_data = f"{name}:{number}".encode()
         PUBKEY.verify(signature, signed_data)
-        return True
-    except Exception as e:
-        st.error(f"Verification failed: {str(e)}")
+    except:
+        st.toast(f"Failed to verify number for {name}!",icon="🚨")
         return False
+    return True
 
 def main():
-    st.title("🔍 Phonebook Lookup (gRPC)")
+    st.title("🔍 Phonebook")
     
     channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = phonebook_pb2_grpc.PhonebookStub(channel)
-
-    st.header("Lookup Entry")
-    name = st.text_input("Name")
-    if st.button("Find"):
+    
+    lookup_tab, add_tab = st.tabs(["Lookup", "Add"])
+    
+    lookup_tab.header("Look up a number")
+    name = lookup_tab.text_input("Name", key="lookup_name")
+    if lookup_tab.button("Find"):
         if name:
             try:
                 lookup_response: LookupResponse = stub.Lookup(LookupRequest(name=name))
                 valid = verify_signature(name, lookup_response.number, lookup_response.signature)
                 if valid:
-                    st.success("✅ Valid signature!")
+                    lookup_tab.badge("Valid signature!", color="green", icon="✅")
                 else:
-                    st.error("❌ Invalid signature")
-                st.success(f"Number: {lookup_response.number}")
-                st.write("Signature valid: ✅" if valid else "❌ Invalid signature!")
+                    lookup_tab.badge("Invalid signature", color="red", icon="❌")
+                lookup_tab.success(f"Number: {lookup_response.number}")
             except grpc.RpcError as e:
-                st.error(f"Error: {e.details()}")
+                st.toast(f"Error: {e}", icon="🚨")
         else:
-            st.warning("Please enter a name")
+            lookup_tab.warning("Please enter a name")
 
-    st.header("Add New Entry")
-    with st.form("add_form"):
-        new_name = st.text_input("Name")
-        new_number = st.text_input("Phone Number")
-        if st.form_submit_button("Add"):
-            if new_name and new_number:
-                try:
-                    add_reponse: AddEntryResponse = stub.AddEntry(
-                        AddEntryRequest(
-                            name=new_name,
-                            number=new_number
-                        ))
-                    st.success(add_reponse.message)
-                except grpc.RpcError as e:
-                    st.error(f"Error: {e}")
-            else:
-                st.error("Both fields are required")
+    add_tab.header("Add a number")
+    add_form = add_tab.form("add_number")
+    new_name = add_form.text_input("Name", key="add_name")
+    new_number = add_form.text_input("Phone Number", key="add_number")
+    if add_form.form_submit_button("Add"):
+        if new_name and new_number:
+            try:
+                add_reponse: AddEntryResponse = stub.AddEntry(
+                    AddEntryRequest(
+                        name=new_name,
+                        number=new_number
+                    ))
+                add_form.success(add_reponse.message)
+            except grpc.RpcError as e:
+                st.toast(f"Error: {e}", icon="🚨")
+        else:
+            st.toast("Both fields are required", icon="🚨")
 
 if __name__ == "__main__":
     main()
